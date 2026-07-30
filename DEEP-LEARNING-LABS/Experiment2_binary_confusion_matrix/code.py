2edac5f9e0063dca19346b07e0a454d8ee0826fb
Dharani Govardhan <palerugopi00@gmail.com>
2026-07-30 13:55:53 +0530

    Add binary classification with confusion matrix visualization
    This script loads the wine dataset, processes it for binary classification, trains a Decision Tree model, and evaluates its performance using various metrics. It also visualizes the binary confusion matrix using a heatmap.

---
+++ DEEP-LEARNING-LABS/Experiment2_binary_confusion_matrix/code.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    classification_report,
+    confusion_matrix
+)
+
+wine = load_wine()
+
+data = pd.DataFrame(data=wine.data, columns=wine.feature_names)
+data["Target"] = wine.target
+
+data = data[data["Target"] != 2]
+
+X = data.drop("Target", axis=1)
+y = data["Target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1
+)
+
+model = DecisionTreeClassifier(random_state=1)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+print("Precision:", precision_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("F1 Score:", f1_score(y_test, y_pred))
+
+cm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(8,6))
+
+sns.heatmap(
+    cm,
+    annot=True,
+    fmt='d',
+    cmap='PuBuGn',
+    xticklabels=wine.target_names[:2],
+    yticklabels=wine.target_names[:2]
+)
+
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Binary Confusion Matrix")
+plt.show()

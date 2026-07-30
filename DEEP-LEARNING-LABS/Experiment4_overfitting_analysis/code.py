2352d0c7b17d11c597f417e80222a16300157538
Dharani Govardhan <palerugopi00@gmail.com>
2026-07-30 14:05:26 +0530

    Create code.py

---
+++ DEEP-LEARNING-LABS/Experiment4_overfitting_analysis/code.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+def true_fun(X):
+    return np.cos(1.5 * np.pi * X)
+
+np.random.seed(0)
+
+n_samples = 30
+
+degrees = [1, 4, 15]
+
+X = np.sort(np.random.rand(n_samples))
+y = true_fun(X) + np.random.randn(n_samples) * 0.1
+
+plt.figure(figsize=(14,5))
+
+for i in range(len(degrees)):
+
+    ax = plt.subplot(1, len(degrees), i + 1)
+
+    plt.setp(ax, xticks=(), yticks=())
+
+    polynomial_features = PolynomialFeatures(
+        degree=degrees[i],
+        include_bias=False
+    )
+
+    linear_regression = LinearRegression()
+
+    pipeline = Pipeline([
+        ("poly", polynomial_features),
+        ("linear", linear_regression)
+    ])
+
+    pipeline.fit(X[:, np.newaxis], y)
+
+    scores = cross_val_score(
+        pipeline,
+        X[:, np.newaxis],
+        y,
+        scoring="neg_mean_squared_error",
+        cv=10
+    )
+
+    X_test = np.linspace(0,1,100)
+
+    plt.plot(
+        X_test,
+        pipeline.predict(X_test[:, np.newaxis]),
+        label="Model"
+    )
+
+    plt.plot(
+        X_test,
+        true_fun(X_test),
+        label="True Function"
+    )
+
+    plt.scatter(
+        X,
+        y,
+        edgecolor="yellow",
+        marker="^",
+        s=20
+    )
+
+    plt.title(
+        f"Degree {degrees[i]}\nMSE={-scores.mean():.2e}"
+    )
+
+plt.show()
